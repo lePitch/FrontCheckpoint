5f54c233c0b7564e21c537655e73eb132fb8fab5
lePitch <pierre.henri.jautard@gmail.com>
2020-07-22 14:57:22 +0200

    structure done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/home/homepage/homepage.component';
+import { HackathonComponent } from './pages/hackathon/hackathon/hackathon.component';
+import { EquipelistComponent } from './pages/hackathon/hackathon/equipelist/equipelist.component';
+import { EquipeComponent } from './pages/equipe/equipe/equipe.component';
+import { DetailsComponent } from './pages/equipe/equipe/details/details.component';
+import { MemberComponent } from './pages/create/member/member.component';
+import { MemberformComponent } from './pages/create/member/memberform/memberform.component';
+import { HackathonlistComponent } from './pages/home/homepage/hackathonlist/hackathonlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HackathonlistComponent,
+    HackathonComponent,
+    EquipelistComponent,
+    EquipeComponent,
+    DetailsComponent,
+    MemberComponent,
+    MemberformComponent
   ],
   imports: [
     BrowserModule,

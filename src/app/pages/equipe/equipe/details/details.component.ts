b6764bce83a2df6d43939de13a59b7aabf88ce68
lePitch <pierre.henri.jautard@gmail.com>
2020-07-22 19:17:01 +0200

    fix: put member

--- src/app/pages/equipe/equipe/details/details.component.ts
+++ src/app/pages/equipe/equipe/details/details.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Member } from 'src/app/models/member/member';
+import { MemberService } from 'src/app/shared/member/member.service';
 
 @Component({
   selector: 'app-details',
@@ -7,13 +8,16 @@ import { Member } from 'src/app/models/member/member';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
-
+  @Output() memberCreated = new EventEmitter<Member>();
   @Input() member: Member;
   show: boolean = false;
-  constructor() { }
+  constructor(private memberService: MemberService) { }
 
   ngOnInit(): void {
   }
   edit(){return this.show = !this.show;}
 
+  parentReception(member: Member){
+    this.memberService.newMember(member);
+}
 }

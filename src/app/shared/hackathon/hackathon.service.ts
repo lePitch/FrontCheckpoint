06449cd2ec8e4783bf69cbde430fe84fe47a92a9
lePitch <pierre.henri.jautard@gmail.com>
2020-07-22 16:33:46 +0200

    feat: hackathon service

--- src/app/shared/hackathon/hackathon.service.ts
+++ src/app/shared/hackathon/hackathon.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Hackathon } from 'src/app/models/hackathon/hackathon';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HackathonService {
 
-  constructor() { }
+  url = 'http://localhost:8080/hackathons/';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Hackathon[]>{
+    return this.http.get<Hackathon[]>(this.url + 'all');
+  }
+  getHackathonById(id): Observable<Hackathon>{
+    return this.http.get<Hackathon>(this.url + id);
+  }
 }

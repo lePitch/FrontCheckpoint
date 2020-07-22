561bebb07dd9d7bde05a44ed0e546987336eb039
lePitch <pierre.henri.jautard@gmail.com>
2020-07-22 15:02:27 +0200

    feat: first routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './pages/home/homepage/homepage.component';
+import { Hackathon } from './models/hackathon/hackathon';
+import { HackathonComponent } from './pages/hackathon/hackathon/hackathon.component';
+import { EquipeComponent } from './pages/equipe/equipe/equipe.component';
+import { MemberComponent } from './pages/create/member/member.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'hackathons', component: HackathonComponent},
+  {path: 'teams', component: EquipeComponent },
+  {path: 'members', component: MemberComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
